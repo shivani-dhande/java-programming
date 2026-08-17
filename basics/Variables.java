7e1da762999596bf9666e12c45f29da2cb9b049a
Shivani Dhande <dhandeshivani05@gmail.com>
2026-08-17 16:48:04 +0530

    Added variables program

---
+++ basics/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 20;
+        double percentage = 85.5;
+        String name = "Shivani";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage);
+    }
+}
