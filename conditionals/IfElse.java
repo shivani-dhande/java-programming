4f00435411b4f87edf0e26ee1c7a0e55683af6f0
Shivani Dhande <dhandeshivani05@gmail.com>
2026-08-18 15:05:59 +0530

    Added if else program

---
+++ conditionals/IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int marks = 75;
+        if (marks >= 40) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
