f195c94f68fb958f26317fc4fb96bad26ede0c72
Shivani Dhande <dhandeshivani05@gmail.com>
2026-08-18 14:46:31 +0530

    Added data types program

---
+++ basics/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int age = 20;
+        double percentage = 85.5;
+        char grade = 'A';
+        boolean passed = true;
+        String name = "Shivani";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+    }
+}
