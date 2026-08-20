ac86683029a62d8dd9f2751b75ecce0c9ddfd751
Shivani Dhande <dhandeshivani05@gmail.com>
2026-08-20 18:21:49 +0530

    Added loop programs

---
+++ loops/MultiplicationTable.java
@@ -0,0 +1,8 @@
+public class MultiplicationTable {
+    public static void main(String[] args){
+            int number=12;
+            for(int j=1;j<=10;j++){
+                System.out.println(number+"x"+j+"="+(number*j));
+            }
+    }
+}
