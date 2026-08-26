b8f79066c2a2d925d5b3218961ad40f42a3bbf09
Shivani Dhande <dhandeshivani05@gmail.com>
2026-08-26 13:55:58 +0530

    Added Java encapsulation

--- oops/Student.java
+++ oops/Student.java
@@ -1,9 +1,9 @@
 public class Student {
-    String name;
-    int age;
-    int marks;
+    private String name;
+    private int age;
+    private int marks;
 
-    Student(String studentName, int studentAge, int studentMarks){
+    public Student(String studentName, int studentAge, int studentMarks){
         name=studentName;
         age=studentAge;
         marks=studentMarks;
@@ -13,5 +13,21 @@ public class Student {
         System.out.println("Name: "+name);
         System.out.println("Age: "+age);
         System.out.println("Marks: "+marks);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getMarks(){
+        return marks;
+    }
+
+    public void setMarks(int marks){
+        if(marks>=0 && marks<=100){
+            this.marks=marks;
+        }else {
+            System.out.println("Invalid marks");
+        }
     }
 }
--- oops/StudentDemo.java
+++ oops/StudentDemo.java
@@ -1,10 +1,16 @@
 public class StudentDemo {
-    public static void  main(String[] args){
+    public static void main(String[] args){
         Student student1= new Student("Shivani", 20, 85);
-        student1.displayInfo();
         Student student2= new Student("Amit", 21, 78);
-        student2.displayInfo();
         Student student3= new Student("Dimple", 20, 88);
+
+        student1.setMarks(150);
+
+        student1.displayInfo();
+        student2.displayInfo();
         student3.displayInfo();
+
+        System.out.println(student1.getName());
+        System.out.println(student1.getMarks());
     }
 }
