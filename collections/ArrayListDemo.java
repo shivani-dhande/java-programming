5e767bca5de9038edf108d6476e0aaca8562c9ff
Shivani Dhande <dhandeshivani05@gmail.com>
2026-09-03 14:33:45 +0530

    Added Java ArrayList programs

---
+++ collections/ArrayListDemo.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class ArrayListDemo {
+    public static void main (String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Shivani");
+        names.add("Amit");
+        names.add("Dimple");
+        System.out.println(names);
+        names.set(0,"Priya");
+        names.remove(1);
+        System.out.println(names.size());
+        for(int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+        System.out.println(names.contains("Priya"));
+        System.out.println(names.contains("Amit"));
+    }
+}
